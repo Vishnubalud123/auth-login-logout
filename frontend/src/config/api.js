68a166da140223da5be66b8a2807aa7c900cae5a
Vishnubalu.D <143685575+Vishnubalud123@users.noreply.github.com>
2025-09-04 21:44:59 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,2 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL || "https://auth-login-logout.onrender.com";
 export default API_URL;
